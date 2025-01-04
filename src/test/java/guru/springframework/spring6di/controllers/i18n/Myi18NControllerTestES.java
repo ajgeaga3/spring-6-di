6cfa95be49400c25e8784e9ce7c13177a4a595d6
aj geaga <ajgeaga1@gmail.com>
2025-01-04 12:02:40 -0800

    Add i18n greeting services with English and Spanish profiles, along with profile-specific tests

---
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/Myi18NControllerTestES.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6di.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("ES")
+class Myi18NControllerTestES {
+
+    @Test
+    void sayHello() {
+    }
+}
