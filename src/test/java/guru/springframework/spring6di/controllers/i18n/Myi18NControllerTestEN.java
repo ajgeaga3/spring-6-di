b4fde3a9daa5c396c8faab9213025dae3a7cc297
aj geaga <ajgeaga1@gmail.com>
2025-01-04 12:08:28 -0800

    Set EnglishGreetingService as the default profile alongside 'EN' for fallback behavior

--- src/test/java/guru/springframework/spring6di/controllers/i18n/Myi18NControllerTestEN.java
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/Myi18NControllerTestEN.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
-@ActiveProfiles("EN")
 @SpringBootTest
 class Myi18NControllerTestEN {
 
