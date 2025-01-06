47a2250090aa934b19319984b991ca9d0c199a0e
aj geaga <ajgeaga1@gmail.com>
2025-01-06 10:07:22 -0800

    Add EnvironmentController with multiple profile-based EnvironmentService implementations

---
+++ src/main/java/guru/springframework/spring6di/services/EnvironmentServiceUAT.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6di.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+@Profile("uat")
+@Service("datasource")
+public class EnvironmentServiceUAT implements EnvironmentService {
+    @Override
+    public String getEnv() {
+        return "uat";
+    }
+}
